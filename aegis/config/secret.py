61863aedf8e2bcac3d47cedae43fca70b15cb610
parkjgy <parkjgy@daam.co.kr>
2019-01-16 03:54:41 +0900

    encrypttion & decryption

--- aegis/config/secret.py
+++ aegis/config/secret.py
@@ -15,62 +15,74 @@ AESkey = "\x3B\x2F\x2A\x20\x4E\x36\x3F\x67\x2D\x6B\x3B\x2C\x3B\x6B\x50\x31\x29\x
 AESiv = "\x31\x47\x42\x75\x42\x7C\x6D\x31\x47\x7B\x22\x5F\x3B\x7B\x2D\x58"
 
 from Crypto.Cipher import AES
+from Crypto.Util.py3compat import *
 from Crypto import Random
 import base64
 
 
+# 기존 PKCS5 -> PKCS7 Padding
+
+def pad(data_to_pad, block_size, style='pkcs7'):
+    if isinstance(data_to_pad, str):
+        data_to_pad = bytes(data_to_pad, encoding='utf-8')
+
+    padding_len = block_size - len(data_to_pad) % block_size
+    if style == 'pkcs7':
+        padding = bchr(padding_len) * padding_len
+    elif style == 'x923':
+        padding = bchr(0) * (padding_len - 1) + bchr(padding_len)
+    elif style == 'iso7816':
+        padding = bchr(128) + bchr(0) * (padding_len - 1)
+    else:
+        raise ValueError("Unknown padding style")
+    return data_to_pad + padding
+
+
+def unpad(padded_data, block_size, style='pkcs7'):
+    pdata_len = len(padded_data)
+    if pdata_len % block_size:
+        raise ValueError("Input data is not padded")
+    if style in ('pkcs7', 'x923'):
+        padding_len = bord(padded_data[-1])
+        if padding_len < 1 or padding_len > min(block_size, pdata_len):
+            raise ValueError("Padding is incorrect.")
+        if style == 'pkcs7':
+            if padded_data[-padding_len:] != bchr(padding_len) * padding_len:
+                raise ValueError("PKCS#7 padding is incorrect.")
+        else:
+            if padded_data[-padding_len:-1] != bchr(0) * (padding_len - 1):
+                raise ValueError("ANSI X.923 padding is incorrect.")
+    elif style == 'iso7816':
+        padding_len = pdata_len - padded_data.rfind(bchr(128))
+        if padding_len < 1 or padding_len > min(block_size, pdata_len):
+            raise ValueError("Padding is incorrect.")
+        if padding_len > 1 and padded_data[1 - padding_len:] != bchr(0) * (padding_len - 1):
+            raise ValueError("ISO 7816-4 padding is incorrect.")
+    else:
+        raise ValueError("Unknown padding style")
+    return padded_data[:-padding_len]
+
+# encrypt ( str or bytes ) return bytes;
 def AES_ENCRYPT(msg):
-    aes = AES.new(bytes(AESkey), AES.MODE_CBC, AESiv)
-
-    msg2 = 32 - (len(msg) % 32)
-    msg2 = '%s%s' % (msg, chr(msg2) * msg2)
-
-    return aes.encrypt(msg2)
+    aes = AES.new(AESkey, AES.MODE_CBC, AESiv)
+    return aes.encrypt(pad(msg, 16))
 
 
-def base64encode(msg):
-    return base64.urlsafe_b64encode(msg).strip(b'=')
+# encrypt ( str or bytes ) return base64:str;
+def AES_ENCRYPT_BASE64(msg):
+    return base64.b64encode(AES_ENCRYPT(msg)).decode(encoding='utf-8')
 
 
-def AES_ENCRYPT_HEX(msg):
-    cipherText = AES_ENCRYPT(msg)
-    r = ''
-    i = 0
-    for ch in cipherText:
-        r += '%02X' % ord(ch) + ' '
-        i += 1
-    return r + 'Length = ' + str(i)
-
-
+# decrypt ( bytes ) return bytes;
 def AES_DECRYPT(msg):
-    try:
-        dec = AES.new(bytes(AESkey), AES.MODE_CBC, AESiv).decrypt(msg)
-        return dec[:-ord(dec[len(dec) - 1])]
-    except Exception as e:
-        logError('ERROR: AES_DECRYPT')
-        logSend('ERROR: AES_DECRYPT')
-        return '......'
+    dec = AES.new(AESkey, AES.MODE_CBC, AESiv).decrypt(msg)
+    return unpad(dec, 16)
 
 
-def base64decode(msg):
-    try:
-        pad = b'=' * (-len(msg) % 4)
-        """
-        logSend('>>> base64decode ' + msg + ' >>> ' + `base64.urlsafe_b64decode(msg + pad)`)
-        msg = msg.replace('-', '+')
-        logSend('   >>> base64decode ' + msg + ' >>> ' + `base64.urlsafe_b64decode(msg + pad)`)
-        """
-        return base64.urlsafe_b64decode(msg + pad)
-    except Exception as e:
-        logError('ERROR: base64decode')
-        logSend('ERROR: base64decode')
-        return 'Li4uLi4u'
-
-
-def AES_DECRYPT_HEX(msg):
-    # logSend('AES_DECRYPT >>>' + `AES_DECRYPT(msg.decode('Hex'))` + '>>>' + `AES_DECRYPT(msg.decode('Hex')).rstrip('\0')`)
-    return AES_DECRYPT(msg.decode('Hex')).rstrip('\0')
-
+# decrypt ( base64:str ) return bytes;
+def AES_DECRYPT_BASE64(msg: str):
+    msg = base64.b64decode(bytes(msg, encoding='utf-8'))
+    return AES_DECRYPT(msg)
 
 """
 Management: testEncryptionStr: get Encryption (Development only)
@@ -80,53 +92,44 @@ http://dev.seole.net:8000/dr/testEncryptionStr?plaintext=1234567890ABCDEFGHIJKLM
 < plainText: 암호되지 않은 문서
 > cipherText: 암호화된 문서
 """
-
-
 @csrf_exempt
 def testEncryptionStr(request):
     logSend('>>> testEncryptionStr: ' + request.META["QUERY_STRING"][10:])
     _plainText = request.META["QUERY_STRING"][10:]
     # _plainText = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ 가나다라마바사아자차카타파하'
 
-    # aaa = _plainText.encode('unicode-escape')
-    # logSend('aaa' + aaa.encode('utf-8'))
-    # bbb = _plainText.decode('unicode-escape')
-    # logSend('bbb' + bbb.encode('utf-8'))
-    # logSend('*****' + _plainText.encode('utf-8'))
-    logSend(_plainText.decode('utf-8'))
+    logSend(_plainText)
     r = '*** plainText = ' + _plainText
+    print('--- ', r)
     _cipherText = AES_ENCRYPT(_plainText)
-    r += '</br>*** cipherText(bytes) = ' + AES_ENCRYPT_HEX(_cipherText)
-    b64cipherText = base64encode(_cipherText)
+    print('--- s 1', _cipherText)
+    r += '</br>*** cipherText(bytes) = ' + _cipherText.hex()
+    print('--- s 2')
+    b64cipherText = AES_ENCRYPT_BASE64(_plainText)
+    print('--- s 3')
     r += '</br>*** base64 cipherText = ' + b64cipherText
-    cipherText = base64decode(b64cipherText)
-    r += '</br>*** cipherText(bytes) = ' + AES_ENCRYPT_HEX(cipherText)
-    plainText = AES_DECRYPT(cipherText)
-    r += '</br>*** replainText = ' + plainText
-    logSend(plainText.decode('utf-8'))
+    plainText = AES_DECRYPT_BASE64(b64cipherText)
+    print('--- s 7')
+    r += '</br>*** replainText = ' + plainText.decode(encoding='UTF-8')
+    print('--- s 8')
+    logSend(plainText)
     return HttpResponse(r)
-
 
 """
 Management: testDecryptionStr: get Decryption (Development only)
    암호화된 문자열을 복호화한다.
-http://localhost:8000/dr/testDecryptionStr?cipherText=bPRlRWGlo2jEJ0o1UuzqFG_-YCFOJLZn9LFk_213X0rbrALoPyF3x1YhRobhUdJCvk13FUA8Xrs4F0aajD3r-4ak9GYxYD97Plo1CAtxtS77Z7rsYFqTak_JKo3pSCTm
-http://dev.seole.net:8000/dr/testDecryptionStr?cipherText=bPRlRWGlo2jEJ0o1UuzqFG_-YCFOJLZn9LFk_213X0rbrALoPyF3x1YhRobhUdJCvk13FUA8Xrs4F0aajD3r-4ak9GYxYD97Plo1CAtxtS77Z7rsYFqTak_JKo3pSCTm
+http://dev.ddtechi.com:8055/dr/testDecryptionStr?cipherText=VAyRZxuerUAjgiDqh9WAx1MvNqb+jCfIN3scgwqUibTfo2eW3AF9DBwebHrCfQ2Eee5u4GhtPL4X1hTnHgKv2Z4T3t67ujIvu7wctmRp1Nidit2XwJjH4q6tLUmTjN+v7ww+T8I1AJH5bI03WgG7NJz4laube8S/rRN7bKzONr2u4QTTRtY0IRZYvxjJxPNRjUJM5nuH7johCspgEToWz/iM+CYmSBVeywfCCQJQAFo=
+http://dev1.ddtechi.com:8033/dr/testDecryptionStr?cipherText=VAyRZxuerUAjgiDqh9WAx1MvNqb+jCfIN3scgwqUibTfo2eW3AF9DBwebHrCfQ2Eee5u4GhtPL4X1hTnHgKv2Z4T3t67ujIvu7wctmRp1Nidit2XwJjH4q6tLUmTjN+v7ww+T8I1AJH5bI03WgG7NJz4laube8S/rRN7bKzONr2u4QTTRtY0IRZYvxjJxPNRjUJM5nuH7johCspgEToWz/iM+CYmSBVeywfCCQJQAFo=
 < cipherText: 암호화된 문서
 > plainText: 복호화된 문서
 """
-
-
 @csrf_exempt
 def testDecryptionStr(request):
     logSend('>>> testDecryptionStr: ' + request.META["QUERY_STRING"][11:])
 
     _b64CipherText = request.META["QUERY_STRING"][11:]
     r = '*** base64 cipherText = ' + _b64CipherText
-    _cipherText = base64decode(_b64CipherText)
-    r += '</br>*** cipherText = ' + AES_ENCRYPT_HEX(_cipherText)
-    plainText = AES_DECRYPT(_cipherText)
-    r += '</br>*** plainText = ' + plainText
-    logSend(plainText.decode('utf-8'))
-    # r += '</br>*** plainText = ' + plainText
+    plainText = AES_DECRYPT_BASE64(_b64CipherText)
+    r += '</br>*** plainText = ' + plainText.decode(encoding='UTF-8')
+    print(plainText.decode(encoding='UTF-8'))
     return HttpResponse(r)
