f8cf2f27fbb21d59e763b8d5b58c8c1a6345666e
shibatokki <tech03042@gmail.com>
2019-01-21 17:03:38 +0900

    Error Global Handler 추가

--- aegis/config/settings/base.py
+++ aegis/config/settings/base.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.error_handler.ProcessExceptionMiddleware',
 ]
 
 CSRF_FAILURE_VIEW = "config.views.csrf_failure"
