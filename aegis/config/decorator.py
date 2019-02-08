bcbc27e832c91bf45af182f3271aa650b87630e5
shibatokki <tech03042@gmail.com>
2019-02-08 11:20:18 +0900

    CROSS_ORIGIN Session 관련 설정

--- aegis/config/decorator.py
+++ aegis/config/decorator.py
@@ -4,16 +4,25 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def cross_origin_read_allow(function):
+    """
+    Cross-Origin 의 Http 상의 접근을 허용하는 Decorator
+    * ORIGIN Header 를 확인하지 못할 경우, Credentials 옵션이 false
+    """
+
     def wrap(request, *args, **kwargs):
         if request.method == 'OPTIONS':
             response = HttpResponse()
         else:
             response = function(request, *args, **kwargs)
-        response["Access-Control-Allow-Origin"] = "*"
+
+        if 'HTTP_ORIGIN' in request.META:
+            response["Access-Control-Allow-Origin"] = request.META['HTTP_ORIGIN']
+            response["Access-Control-Allow-Credentials"] = 'true'
+        else:
+            response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Methods"] = "GET, OPTIONS, POST"
         response["Access-Control-Max-Age"] = "1000"
         response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
-        response["Access-Control-Allow-Credentials"] = True
         return response
 
     wrap.__doc__ = function.__doc__
