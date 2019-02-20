01ffe1663c93f7e32b66dfb66ab83b3ab530e9ff
parkjgy <parkjgy@daam.co.kr>
2019-02-20 21:22:47 +0900

    debugging

--- aegis/operation/views.py
+++ aegis/operation/views.py
@@ -695,6 +695,7 @@ def list_customer(request):
     print(arr_customer)
     op_arr_customer = []
     for customer in arr_customer:
+        print(customer['corp_name'], customer['name'])
         customer['name'] = customer['corp_name']
         op_customer = customer
         del op_customer['id']
