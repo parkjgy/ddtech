3695310150d6bd7cb3001494d9bf7336409ac652
parkjgy <parkjgy@daam.co.kr>
2019-04-19 14:28:10 +0900

    employee  c my_work_histories_for_customer

--- aegis/config/urls.py
+++ aegis/config/urls.py
@@ -31,7 +31,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_apk_upload', confViews.api_apk_upload),  # 업로드 URL
     path('app', confViews.beta_employee_app_download),  # 근로자 앱 다운로드 ( 베타 링크, 의사 결정 필요 )
-    path('apm', confViews.beta_manager_app_download),  # 근로자 앱 다운로드 ( 베타 링크, 의사 결정 필요 )
+    path('apm', confViews.beta_manager_app_download),  # 관리자 앱 다운로드 ( 베타 링크, 의사 결정 필요 )
+    path('tncp', confViews.tnc_privacy),  # 개인정보 보호 약관
     path('android_upload', confViews.app_upload_view),  # 근로자 앱 업로드
     # path('employee/', include('employee.urls')),
 
