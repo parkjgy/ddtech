f03c831fb2da3ea3ec3a8c8b35b40c3618194292
parkjgy <parkjgy@daam.co.kr>
2019-07-28 22:01:33 +0900

    Employee  d work_accept

--- aegis/config/decorator.py
+++ aegis/config/decorator.py
@@ -36,7 +36,7 @@ def cross_origin_read_allow(function):
                     rqst = request.GET
                 # 함수 파라미터 표시
                 for key in rqst.keys():
-                    logSend('^  {}: {} {}'.format(key, rqst[key], AES_DECRYPT_BASE64(rqst[key]) if key in '_id' else ''))
+                    logSend('^  {}: {} {}'.format(key, rqst[key], AES_DECRYPT_BASE64(rqst[key]) if '_id' in key else ''))
                 response = function(request, *args, **kwargs)
                 logSend('<<< {}'.format(get_api(request)))  # 함수 끝 표시
             except Exception as e:
