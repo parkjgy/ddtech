3a474d5907f15403cc7dbbddb25b688b7f1dc1f0
parkjgy <parkjgy@daam.co.kr>
2019-08-07 23:15:59 +0900

    Config  c decorator: cross_origin_read_allow

--- aegis/config/decorator.py
+++ aegis/config/decorator.py
@@ -36,10 +36,16 @@ def cross_origin_read_allow(function):
                     rqst = request.GET
                 # 함수 파라미터 표시
                 for key in rqst.keys():
-                    logSend('^  {}: {} {}'.format(key, rqst[key],
-                                                  AES_DECRYPT_BASE64(rqst[key]) if '_id' in key and
-                                                                                   'n_id' not in key and
-                                                                                   '_id_' not in key else ''))
+                    plain_text = ''
+                    if '_id' in key and 'n_id' not in key and '_id_' not in key:
+                        if len(rqst[key]) > 20: # AES 암호화 된 값이면 22자가 최소임 - 즉 암호화된 값
+                            plain_text = AES_DECRYPT_BASE64(rqst[key])
+                            if plain_text == '__error':
+                                plain_text = ''
+                    logSend('^  {}: {} {}'.format(key, rqst[key], plain_text))
+                                                  # AES_DECRYPT_BASE64(rqst[key]) if '_id' in key and
+                                                  #                                  'n_id' not in key and
+                                                  #                                  '_id_' not in key else ''))
                 response = function(request, *args, **kwargs)
                 logSend('<<< {}'.format(get_api(request)))  # 함수 끝 표시
             except Exception as e:
