c084c69c14f06df3bc190b0addd3a9997821c25b
thinking <thinking@ddtechi.com>
2019-09-03 13:56:10 +0900

    Employee  + reg_from_certification_no: old android version return 200, 201, 202 > 200

--- aegis/employee/views.py
+++ aegis/employee/views.py
@@ -1860,10 +1860,13 @@ def reg_from_certification_no(request):
     # passer.cn = 0
     # passer.dt_cn = None
     passer.save()
-    if len(phone_type) == 1:
-        if 200 <= status_code <= 202:
-            status_code = 200
-            logSend('    ***exchange: 200, 201, 202 to 200')
+    logSend('   *** ({})'.format(phone_type))
+    if phone_type[:1] == 'A':
+        if len(phone_type) == 1:
+            logSend('   *** phone type len: 1')
+            if 200 <= status_code <= 202:
+                status_code = 200
+                logSend('   *** exchange: 200, 201, 202 to 200')
     return StatusCollection(status_code, '정상적으로 처리되었습니다.').to_json_response(result)
     # return REG_200_SUCCESS.to_json_response(result)
 
