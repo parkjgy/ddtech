f8d5bb58983069265b7aed47b9d1072a56526e90
thinking <thinking@ddtechi.com>
2019-12-12 13:40:07 +0900

    Employee  d update_work_for_customer: rqst['time_info']

--- aegis/employee/views.py
+++ aegis/employee/views.py
@@ -608,7 +608,8 @@ def update_work_for_customer(request):
             staff_name=rqst["staff_name"],
             staff_pNo=rqst["staff_pNo"],
         )
-        work.set_time_info(rqst['time_info'])
+        if 'time_info' in rqst:
+            work.set_time_info(rqst['time_info'])
         work.save()
     else:
         work = find_works[0]
@@ -619,7 +620,8 @@ def update_work_for_customer(request):
         work.end = rqst['end']
         work.staff_name = rqst['staff_name']
         work.staff_pNo = rqst['staff_pNo']
-        work.set_time_info(rqst['time_info'])
+        if 'time_info' in rqst:
+            work.set_time_info(rqst['time_info'])
         work.save()
     #
     # 근로자 중에서 업무 날짜가 변경된 근로자의 업무시간을 변경한다.
