9b16146833392fe8866d5890cc61fe4041b04b12
thinking <thinking@ddtechi.com>
2020-03-12 03:41:14 +0900

    Employee  c sms: [????] 이지체크

--- aegis/employee/views.py
+++ aegis/employee/views.py
@@ -2876,7 +2876,7 @@ def exchange_phone_no_to_sms(request):
         'sender': settings.SMS_SENDER_PN,
         'receiver': phone_no,
         'msg_type': 'SMS',
-        'msg': '이지체크 [' + str(certificateNo) + ']\n'
+        'msg': '[' + str(certificateNo) + '] 이지체크\n'
         '인증번호 입니다.'
     }
     if settings.IS_TEST:
