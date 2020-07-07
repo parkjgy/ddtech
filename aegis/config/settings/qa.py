1db57c76594e3089ffb48df350bd5436182c8ad9
naon.lee <naon.lee@linecorp.com>
2020-07-07 10:16:31 +0900

    Modify mysql address for QA

--- aegis/config/settings/qa.py
+++ aegis/config/settings/qa.py
@@ -17,7 +17,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'ddtech',
         'PASSWORD': 'ddTech!!82',
-        'HOST': 'aegisdbinstance.cpa3beafxpw7.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'aegis-qa.cpa3beafxpw7.ap-northeast-2.rds.amazonaws.com',
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',  # Set to empty string for default.
     }
